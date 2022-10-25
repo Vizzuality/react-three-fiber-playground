9bfd89160dc547d604d573752a89c51c6b8daa46
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-10-25 20:03:08 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+import { Box } from "./meshes/box";
 
 function App() {
+
   return (
     <div className="bg-gray-500 w-full h-screen">
       <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="white" position={[0, 0, 5]} />
+        <ambientLight intensity={0.25} />
+        <pointLight color="white" position={[0, 10, 4]} />
 
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
+        <Box />
+
+        <OrbitControls />
       </Canvas>
     </div>
   );
