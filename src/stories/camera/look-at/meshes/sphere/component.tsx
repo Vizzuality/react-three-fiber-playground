dba042fd10f2a4c07b0f3550aa3803c77cc23438
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-12-08 22:47:52 +0100

    Camera: look at

--- src/stories/camera/look-at/meshes/sphere/component.tsx
+++ src/stories/camera/look-at/meshes/sphere/component.tsx
@@ -1,16 +1,12 @@
 import { useFrame, useThree } from "@react-three/fiber";
 import { useRef } from "react";
-import { Mesh, Vector3 } from "three";
+import { Mesh } from "three";
 
 function Sphere() {
   const sphereRef = useRef<Mesh>(null);
   const { camera } = useThree();
 
   useFrame(({ clock }) => {
-    sphereRef.current!.position.x += 0.01;
-    sphereRef.current!.position.y += 0.01;
-
-
     camera.position.x = Math.sin(clock.getElapsedTime()) * 3;
     camera.position.y = Math.sin(clock.getElapsedTime());
     camera.position.z = Math.cos(clock.getElapsedTime()) * 3;
