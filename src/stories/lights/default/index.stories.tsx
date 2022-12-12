c42bb5d055368bcd10ce137267bfcadcf3034755
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-12-12 18:21:02 +0100

    Shadows

---
+++ src/stories/lights/default/index.stories.tsx
@@ -0,0 +1,28 @@
+import { Story } from '@storybook/react/types-6-0';
+import { Canvas } from "@react-three/fiber";
+
+import Lights from "./meshes/lights";
+import Sphere from "./meshes/scene";
+import { OrbitControls } from '@react-three/drei';
+import { VSMShadowMap } from 'three';
+
+const StoryLights = {
+  title: 'Lights',
+};
+
+export default StoryLights;
+
+const Template: Story = () => {
+  return <div className="w-full h-screen bg-gray-500">
+  <Canvas camera={{ position: [1, 2, 5] }} shadows={{ type: VSMShadowMap }}>
+    <Lights />
+    <Sphere />
+
+    <OrbitControls makeDefault />
+  </Canvas>
+</div>;
+};
+
+export const _1_Lights = Template.bind({});
+_1_Lights.args = {
+};
---
+++ src/stories/lights/default/meshes/scene/component.tsx
@@ -0,0 +1,32 @@
+function Scene() {
+  return (
+    <>
+      <mesh castShadow>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="gold" roughness={0.5} metalness={0.5} />
+      </mesh>
+
+      <mesh position={[2,0,0]} castShadow>
+        <torusKnotBufferGeometry args={[0.3, 0.1, 100, 16]} />
+        <meshStandardMaterial color="gold" roughness={0.5} metalness={0.5} />
+      </mesh>
+
+      <mesh position={[-2,0,0]} castShadow>
+        <icosahedronGeometry args={[0.5, 8]} />
+        <meshStandardMaterial color="gold" roughness={0.5} metalness={0.5} />
+      </mesh>
+
+      <mesh castShadow>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="gold" roughness={0.5} metalness={0.5} />
+      </mesh>
+
+      <mesh receiveShadow position={[0, -0.5, 0]} rotation={[-Math.PI/2, 0, 0]}>
+        <planeGeometry args={[100, 100]} />
+        <meshStandardMaterial color="silver" roughness={0.5} metalness={0.5} />
+      </mesh>
+    </>
+  );
+}
+
+export default Scene;
